8f19337ddb4b15ce7f5d32ab218122e0ba94285b
Daniel Compton <desk@danielcompton.net>
2017-07-17 10:48:29 +1200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject co.deps/defn-spec "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+                 [org.clojure/clojurescript "1.9.562"]
+                 [org.clojure/spec.alpha "0.1.123"]])
