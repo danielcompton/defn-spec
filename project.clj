9f40df852779c15ca02d90ffadb48302cd743ec4
danielcompton <desk@danielcompton.net>
2019-01-22 09:23:52 +1300

    Update dependencies

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [org.clojure/clojurescript "1.9.562"]
-                 [org.clojure/spec.alpha "0.1.123"]])
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [prismatic/schema "1.1.10"]
+                 [org.clojure/clojurescript "1.9.562"]]
+  :profiles {:dev {:dependencies [[orchestra "2018.12.06-2"]]}})
