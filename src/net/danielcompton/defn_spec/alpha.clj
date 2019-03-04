0ccd0dceead62055df237910e7c09b58e57d85e0
Daniel Compton <desk@danielcompton.net>
2019-03-05 09:45:18 +1300

    Update defn docstring
    Fixes #9

--- src/net/danielcompton/defn_spec/alpha.clj
+++ src/net/danielcompton/defn_spec/alpha.clj
@@ -1,40 +1,35 @@
 (ns net.danielcompton.defn-spec.alpha
   (:refer-clojure :exclude [defn])
   (:require [clojure.spec.alpha :as s]
-            [schema.core]
             [net.danielcompton.defn-spec.alpha.macros :as macros]))
 
 (defmacro defn
   "Like clojure.core/defn, except that spec typehints can be given on
    the argument symbols and on the function name (for the return value).
 
-   (s/defn foo :- number?
-    [x :- integer?
-     y :- number?]
-    (* x y))
+   (s/defn calculate-total :- number?
+    [qty :- :unit/quantity
+     price :- ::price]
+    (* qty price))
 
    Gotchas and limitations:
-    - The output schema always goes on the fn name, not the arg vector. This
-      means that all arities must share the same output schema. Schema will
-      automatically propagate primitive hints to the arg vector and class hints
+    - The output spec always goes on the fn name, not the arg vector. This
+      means that all arities must share the same :ret spec (as do all Clojure specs).
+      defn-spec will automatically propagate primitive hints to the arg vector and class hints
       to the fn name, so that you get the behavior you expect from Clojure.
-    - All primitive schemas will be passed through as type hints to Clojure,
-      despite their legality in a particular position.  E.g.,
-        (s/defn foo [x :- int])
-      will fail because Clojure does not allow primitive ints as fn arguments;
-      in such cases, use the boxed Classes instead (e.g., Integer).
-    - Schema metadata is only processed on top-level arguments.  I.e., you can
-      use destructuring, but you must put schema metadata on the top-level
+    - spec metadata is only processed on top-level arguments.  I.e., you can
+      use destructuring, but you must put spec metadata on the top-level
       arguments, not the destructured variables.
-      Bad:  (s/defn foo [{:keys [x :- s/Int]}])
-      Good: (s/defn foo [{:keys [x]} :- {:x s/Int}])
+      Bad:  (s/defn foo [{:keys [x :- ::x]}])
+      Good: (s/defn foo [{:keys [x]} :- (s/keys :req [::x])])
     - Only a specific subset of rest-arg destructuring is supported:
       - & rest works as expected
-      - & [a b] works, with schemas for individual elements parsed out of the binding,
-        or an overall schema on the vector
+      - & [a b] works, with specs for individual elements parsed out of the binding,
+        or an overall spec on the vector
       - & {} is not supported.
     - Unlike clojure.core/defn, a final attr-map on multi-arity functions
       is not supported."
+  {:arglists '([name ret-spec? doc-string? attr-map? [params*] prepost-map? body])}
   [& defn-args]
   (let [[name & more-defn-args] (macros/normalized-defn-args &env defn-args)
         {:keys [doc tag] :as standard-meta} (meta name)
