d697b6be1ac8e9ddcc8e2994d3a91036b085e699
Daniel Compton <desk@danielcompton.net>
2019-03-31 12:23:36 +1300

    Set Clojure and ClojureScript dependencies to provided scope

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/danielcompton/defn-spec"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [prismatic/schema "1.1.10"]
-                 [org.clojure/clojurescript "1.9.562"]]
+  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.562" :scope "provided"]
+                 [prismatic/schema "1.1.10"]]
   :profiles {:dev {:dependencies [[orchestra "2019.02.06-1"]]}})
