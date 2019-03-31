8ab39b4453783b6b93c43e8cfb0db3be33ca4066
Daniel Compton <desk@danielcompton.net>
2019-03-31 19:20:44 +1300

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.danielcompton/defn-spec-alpha "0.1.0"
+(defproject net.danielcompton/defn-spec-alpha "0.1.1-SNAPSHOT"
   :description "Create specs for your functions inline with your defn's"
   :url "https://github.com/danielcompton/defn-spec"
   :license {:name "Eclipse Public License"
